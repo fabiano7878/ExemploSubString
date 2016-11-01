1fb6d9e6af7368000b39f011f2de48bb55e32185
Fabiano dos Santos Silva <fabiano.asistemas@gmail.com>
2016-11-01 10:45:32 -0200

    Add files via upload

---
+++ src/ExemploSubString.java
@@ -0,0 +1,17 @@
+
+public class ExemploSubString {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+      /*  MensagemErroEntity msgErro = MensagemErroEntity.getInstance();
+        final int tamanhoCodMensagem = 3;
+        long codigoErro = Long.valueOf(msgRetorno.substring(0, tamanhoCodMensagem));
+        String mensagem = msgRetorno.substring(tamanhoCodMensagem);
+*/
+    }
+    
+    
+
+
+}
